e3ca73fbe28ccbb9fd77afc52a5439e1c2046195
Bishnu <mishra.bishnu554@gmail.com>
2026-01-03 00:06:12 +0530

    Updated api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://3.111.153.106:5000/api";
+//const API_BASE_URL = "http://3.111.153.106:5000/api";
+const API_BASE_URL = "/api";
 //const API_BASE_URL = "http://localhost:5000/api";
 // Create an Axios instance with default configurations
 const apiClient = axios.create({
